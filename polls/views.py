4c91c5b681ba6122ccc36bb700452c62ac6c7c5b
Carrot <xueli.hu@get88.cn>
2017-11-29 23:43:05 +0800

    vote

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404,HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
+
 
 from django.http import HttpResponse
 
-from polls.models import Question
+from polls.models import Question, Choice
 # Create your views here.
 
 # def index(request):
@@ -24,14 +26,27 @@ def index(request):
 # 		question = Question.objects.get(pk=question_id)
 # 	except Question.DoesNotExist:
 # 		raise Http404("Question does not exist")
-# 	return render(request, 'polls/details.html', {"question": question})
+# 	return render(request, 'polls/detail.html', {"question": question})
 def detail(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/details.html', {"question": question})
+	return render(request, 'polls/detail.html', {"question": question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# 重新显示问题的投票表单
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# 成功处理之后 POST 数据之后，总是返回一个 HttpResponseRedirect 。防止因为用户点击了后退按钮而提交了两次。
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
